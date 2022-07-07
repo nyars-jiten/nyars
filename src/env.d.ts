7546c2b71008de208215be1e085d02287921cabd
sawich <sawich@angeldev.org>
2022-07-07 04:33:40 +0300

    cleanup

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,8 @@
 /// <reference types="vite/client" />
 
-type PropsOrPropOptions = object;
-type RawBindings = object;
-type D = object;
-
 declare module "*.vue" {
 	import type { DefineComponent } from "vue";
-	const component: DefineComponent<PropsOrPropOptions, RawBindings, D>;
+	const component: DefineComponent<object, object, object>;
 	export default component;
 }
 
