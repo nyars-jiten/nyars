dd023b6123a5c42b4a00fc678963a33177ad628f
sawich <sawich@angeldev.org>
2022-07-07 03:46:59 +0300

    fix lint env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,11 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+type PropsOrPropOptions = object;
+type RawBindings = object;
+type D = object;
+
+declare module "*.vue" {
+	import type { DefineComponent } from "vue";
+	const component: DefineComponent<PropsOrPropOptions, RawBindings, D>;
+	export default component;
 }
