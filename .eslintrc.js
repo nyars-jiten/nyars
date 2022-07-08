24391e0df94b7b8bf7e3930787463d8ddccbb063
sawich <sawich@angeldev.org>
2022-07-08 23:06:59 +0300

    try elsint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,10 @@ module.exports = {
 		node: true,
 	},
 	extends: [
-		"plugin:vue/vue3-essential",
 		"eslint:recommended",
-		"@vue/typescript/recommended",
+		"plugin:vue/vue3-recommended",
 		"plugin:prettier/recommended",
+		"@vue/typescript/recommended",
 	],
 	parserOptions: {
 		ecmaVersion: "latest",
