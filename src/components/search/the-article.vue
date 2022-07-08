e2265d073e143c82231ee3ffc28ce5add0dd4233
sawich <sawich@angeldev.org>
2022-07-08 19:35:22 +0300

    fix lint

--- src/components/search/the-article.vue
+++ src/components/search/the-article.vue
@@ -3,39 +3,41 @@
 		class="select-text bg-white px-10 py-5 leading-relaxed rounded-md shadow-md border border-gray-100"
 	>
 		<div class="border-b border-b-gray-100 pb-2 mb-2 text-xl">
-			<div v-for="word of article.entry.words">
+			<div v-for="(word, wordId) of article.entry.words" :key="wordId">
 				<span class="before:content-['【'] after:content-['】']">
 					<span
-						v-for="w of word.writings"
+						v-for="(w, writingId) of word.writings"
+						:key="writingId"
 						class="after:content-['・'] last:after:content-none"
 					>
 						{{ w.value }}
 					</span>
 				</span>
 				<span
-					v-for="r of word.readings"
+					v-for="(r, readingId) of word.readings"
+					:key="readingId"
 					class="after:content-['・'] last:after:content-none"
 				>
-					{{ rStore.getByLiteral(r.value) }}
+					{{ store.getByLiteral(r.value) }}
 				</span>
 			</div>
 		</div>
 
 		<!-- {{ mean.pos }} -->
 		<div class="grid grid-cols-[auto_1fr] gap-4">
-			<template v-for="mean of article.entry.meanings">
-				<template v-for="lang of mean.langMeanings">
+			<template v-for="(mean, meanId) of article.entry.meanings" :key="meanId">
+				<template v-for="(lang, langId) of mean.langMeanings" :key="langId">
 					<div class="italic text-gray-400">
 						{{ locale.t(`${MessagesNames.SearchShortLangName}.${lang.lang}`) }}
 					</div>
 
 					<template v-if="lang.senses.length > 1">
 						<div class="grid grid-cols-[auto_1fr] gap-x-2 gap-y-1">
-							<template v-for="(sence, i) of lang.senses">
+							<template v-for="(sence, senseId) of lang.senses" :key="senseId">
 								<div
 									class="text-center border-r border-dotted border-gray-100 pr-2"
 								>
-									{{ 1 + i }}
+									{{ 1 + senseId }}
 								</div>
 								<div>
 									{{ sence.value }}
@@ -64,5 +66,5 @@
 	defineProps<Props>();
 
 	const locale = useI18n();
-	const rStore = useReadingsStorage();
+	const store = useReadingsStorage();
 </script>
