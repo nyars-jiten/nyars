1e52ed305394ef3734908b241af66fa072312c53
sawich <sawich@angeldev.org>
2022-07-11 21:21:31 +0300

    update linter ruls

--- src/core/handwriting/null.ts
+++ src/core/handwriting/null.ts
@@ -1,17 +1,16 @@
 import type { Handwriting } from "./handwriting";
 
 export class Null implements Handwriting {
-	/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	onStart(_: MouseEvent) {
 		return undefined;
 	}
 
-	/* eslint-disable-next-line @typescript-eslint/no-empty-function */
 	onEnd() {
 		return undefined;
 	}
 
-	/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	onDraw(_: MouseEvent) {
 		return undefined;
 	}
