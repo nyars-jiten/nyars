a7b0f813b23e2775e99eeefc702dc7c7d806ae21
Etsune <me@etsu.dev>
2022-07-11 04:23:38 +0300

    tags localization

--- src/locale/langs/tags/rinf.ts
+++ src/locale/langs/tags/rinf.ts
@@ -2,10 +2,16 @@ type Row = "rk" | "gikun" | "oik" | "uK" | "rm" | "ok";
 type Value = { short: string; full: string };
 
 export default {
-	rk: { short: "rk", full: "" },
-	gikun: { short: "gikun", full: "" },
-	oik: { short: "oik", full: "" },
-	uK: { short: "uK", full: "" },
-	rm: { short: "rm", full: "" },
-	ok: { short: "ok", full: "" },
+	rk: { short: "редк", full: "(более) редкое чтение" },
+	gikun: {
+		short: "гикун",
+		full: "гикун (чтение по смыслу) или дзюкудзикун (особенное чтение)",
+	},
+	oik: { short: "△", full: "нестандартная запись каной" },
+	uK: { short: "об.К.", full: "обычно записывается кандзи" },
+	rm: { short: "ошиб", full: "ошибочное, неправильное чтение" },
+	ok: {
+		short: "◇",
+		full: "старая орфография (кю-канадзукай) или архаичное чтение",
+	},
 } as { [key in Row]: Value };
