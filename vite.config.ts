ed6bfdf1c94f216281ee2cfba62b05a09e296ab4
Etsune <me@etsu.dev>
2022-08-02 05:14:38 +0300

    upgrade to vite3; remove sanitize-html

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import vue from "@vitejs/plugin-vue";
-import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 import { defineConfig } from "vite";
@@ -14,16 +13,12 @@ export default defineConfig({
 	server: {
 		port: 8080,
 	},
-	plugins: [
-		vue(),
-		wasm(),
-		topLevelAwait({
-			// The export name of top-level await promise for each chunk module
-			promiseExportName: "__tla",
-			// The function to generate import names of top-level await promise in each chunk module
-			promiseImportName: i => `__tla_${i}`,
-		}),
-	],
+	// temporary solution
+	// while topLevelAwait is not working
+	build: {
+		target: "esnext",
+	},
+	plugins: [vue(), wasm()], //, topLevelAwait()
 	define: {
 		__VUE_I18N_LEGACY_API__: false,
 		__VUE_I18N_FULL_INSTALL__: false,
