7755f4d6e7129dd9beb921c6b82cb23c376d209f
sawich <sawich@angeldev.org>
2022-11-09 15:47:12 +0200

    cleanup

--- src/stores/search copy/types/actions.ts
+++
@@ -1,6 +0,0 @@
-export type Actions = {
-	searchJp(props: { request?: string; userRequest: boolean }): Promise<void>;
-	searchKanji(props: { request?: string; userRequest: boolean }): Promise<void>;
-	clearResults(props: { withGrammar: boolean }): void;
-	searchSuggs({ request }: { request?: string }): Promise<void>;
-};
