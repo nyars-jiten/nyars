6bafd68001991f508ace28cec70ff1cb12f0d64f
sawich <sawich@angeldev.org>
2022-11-10 22:40:43 +0200

    change routes names

--- src/router/routes-names.ts
+++ src/router/routes-names.ts
@@ -1,8 +1,8 @@
 export const enum RoutesNames {
-	SearchHome = "search-home",
-	SearchJapResults = "search/jap",
-	SearchKanResults = "search/kan",
-	DictJpArticle = "jp",
+	SearchHome = "/",
+	SearchJapResults = "s/jap",
+	SearchKanResults = "s/kan",
+	DictJpArticle = "jap",
 	Edits = "edits",
-	DictKanjiArticle = "kanji",
+	DictKanjiArticle = "kan",
 }
