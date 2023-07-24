23ef40d7d6778dd099bd674fd51c7efc4d72524c
etsune <me@etsu.dev>
2023-07-24 05:03:00 +0300

    eslint workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
 	root: true,
 	extends: [
-		"plugin:vue/vue3-essential",
+		"plugin:vue/vue3-recommended",
 		"plugin:tailwindcss/recommended",
 		"eslint:recommended",
 		"@vue/eslint-config-typescript",
