69dbd8dc368111cc0d120fd35039cf7e18aab05e
sawich <sawich@angeldev.org>
2023-07-26 20:53:45 +0300

    Update project configs

---
+++ tailwind.config.mjs
@@ -0,0 +1,103 @@
+import colors from 'tailwindcss/colors'
+
+const statusVariant1 = '#b3b3b3' // New
+const statusVariant2 = '#b31d1d' // Declined
+const statusVariant3 = '#1db336' // Accepted
+const statusVariant4 = '#1d90b3' // Auto-accepted
+const statusVariant5 = '#aa1db3' // Reverted
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        'app-header-2': ['JetBrains Mono', 'monospace'],
+        'app-header': ['Roboto Flex', 'sans-serif'],
+        header: [
+          '"Exo 2"',
+          '"Noto Sans JP"',
+          "'Sukima Gothic Regular'",
+          "'BabelStone Han'",
+          'Arial',
+          'sans-serif'
+        ],
+        body: [
+          '"Exo 2"',
+          '"Noto Sans JP"',
+          "'Sukima Gothic Regular'",
+          "'BabelStone Han'",
+          'Arial',
+          'sans-serif'
+        ]
+      },
+      backgroundImage: {
+        'logo-light': "url('/favicon-96x96.png')",
+        'logo-dark': "url('/favicon-dark-96x96.png')"
+      },
+      colors: {
+        accent: {
+          100: '#e1edee',
+          200: '#c3dadd',
+          300: '#a6c8cd',
+          400: '#88b5bc',
+          500: '#6aa3ab',
+          600: '#558289',
+          700: '#406267',
+          800: '#2a4144',
+          900: '#152122'
+        },
+
+        gray: colors.zinc,
+
+        'tag-common': colors.gray,
+        'tag-jyouyou': colors.slate,
+        'tag-insatsu': colors.slate,
+        'tag-jinmeiyou': colors.slate,
+        'tag-jlpt': colors.amber,
+        'tag-kanken': colors.pink,
+        'tag-grade': colors.teal,
+        'tag-shou': colors.teal,
+        'tag-chuu': colors.amber,
+        'tag-kou': colors.orange,
+        'tag-kanon': colors.slate,
+        'tag-souon': colors.slate,
+        'tag-touon': colors.slate,
+        'tag-goon': colors.slate,
+        'tag-kanyoon': colors.slate,
+        'tag-gai': colors.blue,
+
+        'status-variant-1': statusVariant1,
+        'status-variant-2': statusVariant2,
+        'status-variant-3': statusVariant3,
+        'status-variant-4': statusVariant4,
+        'status-variant-5': statusVariant5,
+
+        'type-variant-1': '#1db336', // New
+        'type-variant-2': '#1d90b3', // Edit
+        'type-variant-3': '#b31d1d', // Delete
+        'type-variant-4': '#a520c7', // Transfer
+        'type-variant-5': '#aa1db3' // Reverted
+      }
+    }
+  },
+  plugins: [],
+  safelist: [
+    "before:content-['「']",
+    "after:content-['」']",
+
+    "before:content-['【']",
+    "after:content-['】']",
+
+    { pattern: /bg-status-variant-/ },
+    { pattern: /border-status-variant-/ },
+    { pattern: /bg-type-variant-/ },
+    { pattern: /text-type-variant-/ },
+    { pattern: /text-status-variant-/ },
+    { pattern: /text-tag-/ },
+    { pattern: /border-tag-/ }
+  ]
+}
+
+// '"Kaisei Tokumin"',
--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,78 @@
-import vue from "@vitejs/plugin-vue";
-import wasm from "vite-plugin-wasm";
-import i18n from "@intlify/vite-plugin-vue-i18n";
+import vue from '@vitejs/plugin-vue'
+import wasm from 'vite-plugin-wasm'
+import router from 'unplugin-vue-router/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
-import { defineConfig } from "vite";
+import locale from '@intlify/unplugin-vue-i18n/vite'
+import components from 'unplugin-vue-components/vite'
+import auto from 'unplugin-auto-import/vite'
+import icons from 'unplugin-icons/vite'
+import resolver from 'unplugin-icons/resolver'
 
-// https://vitejs.dev/config/
+import {
+  VueUseComponentsResolver,
+  HeadlessUiResolver,
+  VueUseDirectiveResolver
+} from 'unplugin-vue-components/resolvers'
+
+import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
+
 export default defineConfig({
-	resolve: {
-		alias: {
-			"@": new URL("src", import.meta.url).pathname,
-		},
-	},
-	build: {
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('src', import.meta.url))
+    }
+  },
+  build: {
     target: 'esnext'
-	},
-	server: {
-		port: 8080,
-	},
-	plugins: [
-		vue(),
-		wasm(),
-		i18n({
-			compositionOnly: true,
-			fullInstall: true,
-		}),
-	],
-});
+  },
+  server: {
+    port: 8080,
+    strictPort: true
+  },
+  plugins: [
+    router({
+      routesFolder: 'src/pages',
+      dts: './src/typings/auto-router.d.ts'
+    }),
+    vue({
+      script: {
+        defineModel: true,
+        propsDestructure: true
+      }
+    }),
+    wasm(),
+    icons({ autoInstall: true }),
+    components({
+      dts: './src/typings/auto-components.d.ts',
+      resolvers: [
+        VueUseComponentsResolver(),
+        HeadlessUiResolver(),
+        VueUseDirectiveResolver(),
+        resolver({ prefix: '' })
+      ]
+    }),
+    auto({
+      imports: [
+        'vue',
+        {
+          vue: ['defineModel']
+        },
+        VueRouterAutoImports,
+        {
+          'vue-router/auto': ['useLink']
+        },
+        'pinia',
+        'vue-i18n',
+        '@vueuse/core',
+        '@vueuse/head'
+      ],
+      dts: './src/typings/auto-imports.d.ts'
+    }),
+    locale({
+      compositionOnly: true,
+      fullInstall: true
+    })
+  ]
+})
