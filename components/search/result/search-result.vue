9f321dcb4447c53ca601436440d42789e4a36a5b
etsune <me@etsu.dev>
2024-10-10 22:26:29 +0300

    status border color

--- components/search/result/search-result.vue
+++ components/search/result/search-result.vue
@@ -38,7 +38,7 @@ const { t } = useI18n()
 
 <template>
   <button type="button" class="w-full text-left" @click="wid = article.wid">
-    <UiBlock :hover="true" class="space-y-2">
+    <UiBlock :hover="true" :nonreviewed="!article.status.isReviewed" :unconfirmed="article.status.isUnconfirmed" class="space-y-2">
       <header class="grid grid-cols-[1fr_auto] items-center justify-between gap-4 text-2xl">
         <span class="grow truncate">
           <span v-for="furigana, wIndex of article.furigana.toSorted((a, b) => a.isMarked ? -1 : 1)" :key="wIndex" class="after:content-['・'] last:after:content-none dark:after:text-gray-700">
--- components/ui/ui-block.vue
+++ components/ui/ui-block.vue
@@ -5,10 +5,14 @@ withDefaults(defineProps<Props>(), {
   size: 'medium',
   hover: false,
   active: false,
+
+  nonreviewed: false,
+  unconfirmed: false,
+  nonstd: false,
 })
 
 const styles = tv({
-  base: 'rounded-md p-4 shadow-md outline outline-1 outline-neutral-800 transition-colors',
+  base: 'rounded-md border-2 border-transparent p-4 shadow-md outline outline-1 outline-neutral-800 transition-colors',
   variants: {
     size: {
       small: 'p-2',
@@ -21,6 +25,15 @@ const styles = tv({
     active: {
       true: 'bg-neutral-800 outline-neutral-700',
     },
+    nonreviewed: {
+      true: 'border-l-amber-700',
+    },
+    unconfirmed: {
+      true: 'border-l-red-700',
+    },
+    nonstd: {
+      true: 'border-l-fuchsia-500',
+    },
   },
 })
 
@@ -28,11 +41,16 @@ interface Props {
   size?: VariantProps<typeof styles>['size']
   hover?: VariantProps<typeof styles>['hover']
   active?: VariantProps<typeof styles>['active']
+
+  // Article props
+  nonreviewed?: VariantProps<typeof styles>['nonreviewed']
+  unconfirmed?: VariantProps<typeof styles>['unconfirmed']
+  nonstd?: VariantProps<typeof styles>['nonstd'] // archaic, dialect, proper
 }
 </script>
 
 <template>
-  <section :class="styles({ size, hover, active })">
+  <section :class="styles({ size, hover, active, nonreviewed, unconfirmed, nonstd })">
     <slot />
   </section>
 </template>
