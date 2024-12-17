5a4dc762ca5c35edf7de425dc52e378c7232a553
etsune <me@etsu.dev>
2024-12-17 22:39:02 +0300

    not implemented page

---
+++ components/NotImplemented.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<template>
+  <div class="flex min-h-full select-text flex-col items-center justify-center gap-4">
+    <span class="text-5xl text-ns-500">{{ t('pages.development.title') }}</span>
+    <span>{{ t('pages.development.text') }}</span>
+    <img
+      src="@/assets/img/16.png"
+      alt="under-dev-image"
+      class="size-72"
+    >
+  </div>
+</template>
