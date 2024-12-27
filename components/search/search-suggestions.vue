a419dd643a52eeaa49c7c7027eec37e80acaa87e
sawich <sawich@angeldev.org>
2024-12-27 23:47:30 +0200

    Fixed search dropdown suggestion list

--- components/search/search-suggestions.vue
+++ components/search/search-suggestions.vue
@@ -32,7 +32,7 @@ watchDebounced(request, () => {
   <section v-if="data && data.length" class="absolute left-0 top-full z-10 w-full pt-4 leading-none text-zinc-300">
     <div class="rounded-md bg-zinc-800/50 p-2 shadow-md outline outline-1 outline-zinc-700 backdrop-blur-md">
       <button v-for="val, index of data" :key="index" type="button" class="inline-flex w-full items-center gap-2 rounded-md px-4 py-1.5 text-start transition-colors hover:bg-zinc-900" @click="push(escapeVal(val))">
-        <div class="grow truncate">
+        <div class="grow truncate leading-6">
           {{ val }}
         </div>
 
