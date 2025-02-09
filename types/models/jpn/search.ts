0c52610f21b656c4b1223d7191b5a2cb73d99840
etsune <me@etsu.dev>
2025-02-09 11:12:30 +0300

    parser api models

--- types/models/jpn/search.ts
+++ types/models/jpn/search.ts
@@ -1,3 +1,5 @@
+import type { FuriganaPair } from './entry'
+
 export interface SearchRequest {
   query: string
 }
@@ -6,7 +8,23 @@ export interface SearchResponse {
   result: EntryJp[]
 }
 
+export interface Token {
+  surface: string
+  furigana: FuriganaPair[]
+  // reading: string
+  deinflect: Deinflect[]
+  children: Token[]
+}
+
+export interface Deinflect {
+  source: string
+  target: string
+  surface: string
+  rule: string
+}
+
 export interface JpnSearchResponse {
   result: EntryJp[]
   request: string[]
+  parsed: Token[]
 }
