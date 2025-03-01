842cd998a7dcab2879fca4b0fcf5fccae91be443
etsune <me@etsu.dev>
2025-03-01 17:18:13 +0300

    fix: check user session

--- app/stores/userStore.ts
+++ app/stores/userStore.ts
@@ -2,8 +2,10 @@ export const useUserStore = defineStore('user-store', () => {
   const { current } = useUser()
   const { serverGetCurrentUser } = useUserRepo()
   const { data: user } = current()
+  const updTime = ref(Date.now())
 
   const checkAccess = (access: Access) => {
+    checkAuth()
     return user?.value && (user.value.isAdmin || ((user.value.access & access) === access))
   }
 
@@ -13,7 +15,14 @@ export const useUserStore = defineStore('user-store', () => {
   } as UserRights))
 
   async function $reset() {
+    updTime.value = Date.now()
     user.value = await serverGetCurrentUser()
+  }
+
+  async function checkAuth() {
+    if (Date.now() - updTime.value > (1 * 60 * 1000)) { // every 15 min 
+      $reset()
+    }
   }
 
   const menuState = ref(true)
